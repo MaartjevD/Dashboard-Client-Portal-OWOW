ef72a25ca186e83e6d09c9227e3d022a403b2f3b
IamErfanImmortal <erf.developer88@gmail.com>
2026-04-06 01:15:48 +0200

    Update Milestones

--- src/client/components/sections/MilestonesCard.jsx
+++ src/client/components/sections/MilestonesCard.jsx
@@ -1,17 +1,19 @@
-function MilestonesCard({ milestones }) {
+import { formatDate } from "../../utils/projectHelpers";
+
+function MilestonesCard({ milestones = [] }) {
   return (
     <section className="mb-3">
       <div className="custom-card milestones-card">
         <div className="milestones-header">
           <i className="bi bi-arrow-up-right"></i>
-          <h3>Aankomende milestones</h3>
+          <h3>Upcoming Milestones</h3>
         </div>
 
         <div className="row g-4">
           {milestones.map((item) => (
             <div className="col-12 col-md-6 col-xl-4" key={item.id}>
               <div className="milestone-small-card">
-                <p className="milestone-date">{item.dueDate}</p>
+                <p className="milestone-date">{formatDate(item.dueDate)}</p>
                 <h4 className="milestone-title">{item.title}</h4>
                 <p className="milestone-subtitle">{item.subtitle}</p>
               </div>
